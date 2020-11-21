36293ccc6c90fbd5602a1b9006cddd20b3506c72
Gabriel da Costa Valin <59366369+Gabriel-Valin@users.noreply.github.com>
2020-11-20 22:54:05 -0300

    update sql final

--- sql/solutudo_db.sql
+++ sql/solutudo_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 20-Nov-2020 às 08:45
+-- Tempo de geração: 21-Nov-2020 às 02:51
 -- Versão do servidor: 10.4.14-MariaDB
 -- versão do PHP: 7.4.10
 
@@ -49,8 +49,7 @@ INSERT INTO `empresas` (`id`, `nome`, `telefone`, `endereco`, `email`, `estado`,
 (28, 'Super Mercado BOA COMPRA', '(28) 3815-9087', 'Avenida Major Nicolau, 790', 'supermercado@boacompra.com', 'ES', 'Vila Velha'),
 (29, 'Ale Combustíveis', '(86) 3008-9876', 'Avenida Principal, 8', 'alecombustiveis@petrobras.com', 'PI', 'Piauí'),
 (30, 'Cencosud', '(89) 3006-9983', 'Centro Principal, 809', 'cencosud@contato.com', 'PI', 'Piauí'),
-(31, 'NWB', '(14) 9999-99999', 'Rua Presidente Do Brasil, 101', 'nwbenterprise@gmail.com', 'SP', 'São Paulo'),
-(32, 'NWB12', '(14) 9999-99999', 'Rua Presidente Do Brasil, 101', 'nwbenterprise@gmail.com', 'SP', 'São Paulo');
+(31, 'NWB', '(14) 9999-99999', 'Rua Presidente Do Brasil, 101', 'nwbenterprise@gmail.com', 'SP', 'São Paulo');
 
 -- --------------------------------------------------------
 
@@ -75,15 +74,12 @@ CREATE TABLE `membros` (
 --
 
 INSERT INTO `membros` (`id`, `nome`, `celular`, `endereco`, `email`, `senha`, `data_admissao`, `foto_membro`, `nivel_acesso`) VALUES
-(31, 'Gabriel Valin', '(14) 99692-8608', 'Rua Pedro Filho de Barros, Jd Nicota de Barros, 101', 'gabrielvalincontato@gmail.com', '$2y$12$lAQb5iZ/io2g6Wgqcm93ruioSwiZ/3NSQjkkRklfI5OsPWNxi6Xru', '20/02/2019', '', 'Gerente'),
 (32, 'Bruno Montanha', '(14) 9982-8889', 'Vila Maria, 208', 'desenvolvimento@solutudo.com', '$2y$10$P8XdgyeZUZtL6eSmIgfrm.G0Bq/qlrmQ0QhfLGTGrGhTU4SK060TG', '19/11/2019', '', 'Gerente'),
 (33, 'John Doe', '(14) 9999-9999', 'Jardim Cristina, 202', 'johndoe@gmail.com', '$2y$10$CLbI61sSvCL6Nc06xf8ZC.mEZXw4lDcZjxsqe.FLg5QcMYQRtcA4G', '18/07/2020', '', 'Vendedor'),
 (34, 'Thais Fernandes', '(14) 9978-7677', 'Comerciários 3, 101', 'thaisfernandes@gmail.com', '$2y$10$r2eQONOP9WKtJMaTXysY8uaa/LNuQcR/ZbfIRxbbivRVufAQ.dzKK', '30/08/2020', '', 'Vendedor'),
 (35, 'Murilo Stanzione', '(14) 9997-4889', 'Vila Maria, 308', 'murilo@gmail.com', '$2y$10$VW9J7uu7A/eUQ5Qh8g1MbOJ9W4EcTAhFI6rkZPq07yfWDSRzl55YC', '20/03/2020', '', 'Vendedor'),
 (36, 'Beatriz Costa', '(14) 9878-6868', 'Avenida Santana, 193', 'beatrizcosta@gmail.com', '$2y$10$hLiwCtva9Ndtf2G3YaAMaOfgNLB8tAaDU0qU/APYD3DBJZm6E0DKm', '16/04/2020', '', 'Vendedor'),
-(37, 'Thiago Stanzione', '(14) 9878-6787', 'Jardim Nicota de Barros, 250', 'thiagostanzione@gmail.com', '$2y$10$8CghNb8LH8moFOKN885t2u0mAvqy4RIqSsvcJdw5JCnPJg9NAVkr.', '05/01/2020', '', 'Vendedor'),
-(38, 'Gabriel Valin', '(14) 99692-8608', 'Rua Pedro Filho de Barros, Jd Nicota de Barros, 101', 'gabrielvalincontato@gmail.com', '$2y$12$J3Bnp66PN2iT1APDRI6vUOphyQoqMMP68oRZ//9UHMnLQU1I8v6Pu', '20/02/2019', '', 'Gerente'),
-(39, 'Gabriel Da Costa', '(32) 1983-2981', 'dasdsa', 'valingabriel0@gmail.com', '$2y$10$SokCn/tuyBDk.quxC0iARO2ZiJY7epFbXePZ4isnOMp2vKxaiJSfS', '19/11/2019', '', 'Gerente');
+(37, 'Thiago Stanzione', '(14) 9878-6787', 'Jardim Nicota de Barros, 250', 'thiagostanzione@gmail.com', '$2y$10$8CghNb8LH8moFOKN885t2u0mAvqy4RIqSsvcJdw5JCnPJg9NAVkr.', '05/01/2020', '', 'Vendedor');
 
 -- --------------------------------------------------------
 
@@ -102,7 +98,7 @@ CREATE TABLE `planos` (
 --
 
 INSERT INTO `planos` (`id`, `planos`, `valor`) VALUES
-(2, 'Plano Prata', 'R$ 600,00'),
+(2, 'Plano Prata', 'R$ 650,00'),
 (3, 'Plano Ouro', 'R$ 900,00'),
 (4, 'Plano Platina', 'R$ 1200,00'),
 (5, 'Plano Bronze', 'R$ 450,00'),
@@ -184,7 +180,7 @@ ALTER TABLE `membros`
 -- AUTO_INCREMENT de tabela `planos`
 --
 ALTER TABLE `planos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT de tabela `vendas`
